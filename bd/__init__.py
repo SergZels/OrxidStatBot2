8b4b0b633ce0c0f9f69bfdebc431ec1a3e5c8d6f
SergZels <92048923+SergZels@users.noreply.github.com>
2023-05-02 09:58:42 +0300

    fix bug

--- bd/__init__.py
+++
@@ -1,1 +0,0 @@
-from bd import bd
--- keyboards/client_keyboard.py
+++ keyboards/client_keyboard.py
@@ -1,10 +1,15 @@
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
 
-b1 = KeyboardButton("/Внести_день")
-b2 = KeyboardButton("/Внести_місяць")
+
+b1 = KeyboardButton("/Виручка_до_обіду")
+b2 = KeyboardButton("/Виручка_після_обіду")
 b3=KeyboardButton("/Стат_Місяць")
 b4=KeyboardButton("/Стат_Рік")
 
 kbcl = ReplyKeyboardMarkup(resize_keyboard=True)
 kbcl.add(b1,b2).add(b3,b4)
+
+markup = ReplyKeyboardMarkup(resize_keyboard=True, row_width=4)
+
+markup.add(('Виручка до обіду 💵'), 'Виручка після обіду 💶').add('Статистика за місяць 📊','Видатки').add('Минулий місяць')
